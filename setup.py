fd3807d8d33c92039d87d1d0989c05fd20586f70
Csaba Palankai <csaba.palankai@gmail.com>
2015-08-28 14:07:03 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import os
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='pyeventstore',
+    author='Made.com',
+    author_email='coders@made.com',
+    packages=find_packages(),
+    include_package_data=True,
+    version='0.1',
+    description="Python Eventstore Client",
+    long_description=read('README.rst'),
+    url='https://github.com/madedotcom/pyeventstore',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Operating System :: OS Independent',
+        'Topic :: Database',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ],
+    keywords=('eventstore', ),
+    zip_safe=False,
+    install_requires=[r for r in read("requirements.txt").split("\n") if r],
+)
